7b69f6506138adba67e203adeb9b6fcc04d81491
restucode <restu.kersana1@gmail.com>
2026-01-15 11:27:00 +0700

    add metadata base on root provider

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -3,6 +3,13 @@ import { ReactNode } from "react";
 import { base } from "wagmi/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import "@coinbase/onchainkit/styles.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '69683f8c4991800a6d9d63e3',
+  },
+};
 
 export function RootProvider({ children }: { children: ReactNode }) {
   return (
